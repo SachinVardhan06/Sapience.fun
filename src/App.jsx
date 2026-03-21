47069049d0fd2730ed1fe69185434494f49f5345
ljrklcjqh <ljrklcjqh@users.noreply.github.com>
2026-03-22 00:50:53 +0530

    Initial sapience.fun coming soon page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import CommingSoon from './pages/comingsoon/commingsoon'
-import { Navigate, Route, Routes } from 'react-router-dom'
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/access" element={<CommingSoon />} />
-      <Route path="/" element={<Navigate to="/access" replace />} />
-      <Route path="*" element={<Navigate to="/access" replace />} />
-    </Routes>
-  )
+  return <CommingSoon />
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/access" element={<App />} />
+        <Route path="/" element={<Navigate to="/access" replace />} />
+        <Route path="*" element={<Navigate to="/access" replace />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
 )
