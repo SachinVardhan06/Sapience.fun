2b51cef1e3c72202a5b3fb367ef8b4eb357c1a7a
ljrklcjqh <ljrklcjqh@users.noreply.github.com>
2026-03-22 00:42:44 +0530

    Initial sapience.fun coming soon page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import CommingSoon from './pages/comingsoon/commingsoon'
+import { Navigate, Route, Routes } from 'react-router-dom'
 
 function App() {
-  return <CommingSoon />
+  return (
+    <Routes>
+      <Route path="/access" element={<CommingSoon />} />
+      <Route path="/" element={<Navigate to="/access" replace />} />
+      <Route path="*" element={<Navigate to="/access" replace />} />
+    </Routes>
+  )
 }
 
 export default App
